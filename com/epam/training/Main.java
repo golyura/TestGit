cbbef79d84814b9981d732158519718158dc99d9
Yura <gol.y@>
2022-04-25 11:06:39 +0300

    Change in main.java - develop

--- com/epam/training/Main.java
+++ com/epam/training/Main.java
@@ -3,5 +3,7 @@ package by.epam.training;
 public class Main {
     public static void main(String[] args) {
           System.out.println("Hello first commit");
+		  System.out.println("Hello from develop");
+
     }
 }
