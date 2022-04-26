a55a856eb17615ffae6bfb7b5905f055cf0fe6b8
Yura <gol.y@mail.ru>
2022-04-26 14:59:47 +0300

    Changes main.java

--- com/epam/training/Main.java
+++ com/epam/training/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
           System.out.println("Hello first commit");
 		  System.out.println("Hello from develop");
-
+// Added new comment
     }
 }
